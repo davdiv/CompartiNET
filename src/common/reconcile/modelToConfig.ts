7e26f8937d4a6d696735b70f6657fda9253e853f
DivDE <divde@musicociel.fr>
2026-05-16 05:30:25 +0000

    Fix modelToConfig

--- src/common/reconcile/modelToConfig.ts
+++ src/common/reconcile/modelToConfig.ts
@@ -1,10 +1,28 @@
 import { reconcile } from ".";
-import { NetworkCreateAction } from "../model/actions";
-import { newNetworkModel } from "../model/actions/namespace";
+import { applyAction, NetworkCreateAction } from "../model/actions";
+import { CreateNamespaceAction, newNetworkModel } from "../model/actions/namespace";
 import { getHardwareMatchActions } from "../model/hardware";
 import { NetworkModel } from "../model/networkModel";
 
 export const modelToConfig = (model: NetworkModel) => {
-  const resolution = reconcile(newNetworkModel(), model);
-  return [...getHardwareMatchActions(model), ...(resolution.actions as NetworkCreateAction[])];
+  const config: NetworkCreateAction[] = [];
+  const baseModel = newNetworkModel();
+  const hardwareMatchActions = getHardwareMatchActions(model);
+  const hardwareNetns = new Set<string>();
+  for (const { netns } of hardwareMatchActions) {
+    hardwareNetns.add(netns);
+  }
+  hardwareNetns.delete(""); // never create the default netns
+  for (const netns of hardwareNetns) {
+    const action: CreateNamespaceAction = { type: "CreateNamespace", netns };
+    config.push(action);
+    applyAction(baseModel, action);
+  }
+  config.push(...hardwareMatchActions);
+  for (const hardwareAction of hardwareMatchActions) {
+    applyAction(baseModel, hardwareAction);
+  }
+  const resolution = reconcile(baseModel, model);
+  config.push(...(resolution.actions as NetworkCreateAction[]));
+  return config;
 };
